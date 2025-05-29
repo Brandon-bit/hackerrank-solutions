9b9dc2a5e0884b674f5bbccd56c1dc50ba110d56
Brandon Josue <brandonsalinas792@gmail.com>
2025-05-29 16:44:03 -0600

    .

---
+++ sql/easy/exercise-3.sql
@@ -0,0 +1,5 @@
+-- Select All - Easy
+
+-- Query all columns (attributes) for every row in the CITY table.
+
+SELECT * FROM CITY
---
+++ sql/easy/exercise-7.sql
@@ -0,0 +1,8 @@
+-- Title: Weather Observation Station 1
+-- Difficult: Easy
+-- link: https://www.hackerrank.com/challenges/weather-observation-station-1
+
+-- Description: 
+-- Query a list of CITY and STATE from the STATION table.
+
+SELECT CITY, STATE FROM STATION
